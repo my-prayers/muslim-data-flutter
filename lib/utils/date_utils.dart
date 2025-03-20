b6251972a0e1ab15705fd02c89d8269cd2c65056
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-03-20 21:00:09 +0300

    refactor: Remove intl package and do the formatting manually

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -1,10 +1,9 @@
-import 'package:intl/intl.dart';
-
 extension DateUtils on DateTime {
   /// Format date to match the database date style.
   /// Pattern: MM-dd
   String toDbDate() {
-    final DateFormat formatter = DateFormat('MM-dd', 'en');
-    return formatter.format(this);
+    final String month = this.month.toString().padLeft(2, '0');
+    final String day = this.day.toString().padLeft(2, '0');
+    return '$month-$day';
   }
 }
--- test/utils/date_utils_test.dart
+++ test/utils/date_utils_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:muslim_data_flutter/utils/date_utils.dart';
 
 void main() {
-  setUp(() {
-    initializeDateFormatting('en', null);
-  });
-
   test('formatToDbDate formats date to MM-dd', () {
     final date = DateTime(2024, 10, 5);
     expect(date.toDbDate(), '10-05');
