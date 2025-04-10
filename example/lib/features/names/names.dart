5d4c203cffe4996a98501dda32b611f8dd47aeb8
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-04-10 21:45:59 +0300

    feat: Add Names of Allah screen to the example app

--- example/lib/features/names/names.dart
+++ example/lib/features/names/names.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:muslim_data_flutter/muslim_data_flutter.dart';
 
 class Names extends StatelessWidget {
   const Names({super.key});
@@ -6,8 +7,30 @@ class Names extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Names')),
-      body: Center(child: Text('99 Names of Allah')),
+      appBar: AppBar(title: const Text('99 Names of Allah')),
+      body: FutureBuilder<List<NameOfAllah>>(
+        future: MuslimRepository().getNames(Language.en),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No data found'));
+          }
+          final names = snapshot.data!;
+          return ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              final currentName = names[index];
+              return ListTile(
+                title: Text(currentName.name),
+                subtitle: Text(currentName.translation),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
