3b980543f7e67f90c2b854039b7ad66c6d2d6894
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-07-17 17:52:42 +0300

    refactor(bump version): Update version bump script to include pub get
    Modified the bump_version.sh script to run 'flutter pub get' after updating the version in pubspec.yaml. Changed git add to include all files for committing changes.

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -12,10 +12,11 @@ commit_msg="build(release): update version to $1 and regenerate changelog"
 
 # Bump files for the new release
 sed -i '' "s/version: .*/version: $1/" pubspec.yaml
+flutter pub get
 git cliff --unreleased --tag $1 --prepend CHANGELOG.md
 
 # Commit changes.
-git add pubspec.yaml CHANGELOG.md
+git add .
 git commit -m "$commit_msg"
 
 # Create the release tag
