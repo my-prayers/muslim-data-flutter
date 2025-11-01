82cd31f4cf23a7a32bdb1bf0cd2cdb42ecb90c26
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-11-01 10:18:08 +0300

    fix: Add CustomMethod class export to the models
    This change adds the export for the custom_method model, allowing it to be accessed from the main library. This enhances the functionality related to prayer time calculations.

--- lib/muslim_data_flutter.dart
+++ lib/muslim_data_flutter.dart
@@ -8,6 +8,7 @@ export 'src/data/models/location/location.dart';
 export 'src/data/models/names/names_of_allah.dart';
 export 'src/data/models/prayer_times/asr_method.dart';
 export 'src/data/models/prayer_times/calculation_method.dart';
+export 'src/data/models/prayer_times/custom_method.dart';
 export 'src/data/models/prayer_times/higher_latitude_method.dart';
 export 'src/data/models/prayer_times/prayer_attribute.dart';
 export 'src/data/models/prayer_times/prayer_time.dart';
